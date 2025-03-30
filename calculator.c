12a33b5571d45e264444770b58003c35e1fcf88b
KirtiJha <kirti.jha2025@gmail.com>
2025-03-30 15:31:39 +0530

    added all  basic operations

--- calculator.c
+++ calculator.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include<math.h> //power fun inside this fun
 
 void print_menu(); // function(or ab sara menu isme add kr denge ...soo our main fun will more small)
 
@@ -7,22 +8,59 @@ int main()
     int choice;
     double first, second, result;
 
-    while(1) {
+    while (1)
+    {
         print_menu();
         scanf("%d", &choice);
-if( choice == 7){
-    break;
-}
+
+        if (choice == 7)
+        {
+            break;
+        }
+
         printf("\n Please enter the first number: ");
-        scanf("%lf", &first); //double = lf, float = f
+        scanf("%lf", &first); // double = lf, float = f
 
         printf("Now, enter the second number: ");
         scanf("%lf", &second);
+
+        switch (choice)
+        {
+        case 1:
+        result = first + second; //add
+            break;
+
+        case 2:
+        result = first - second; //subtract
+            break;
+
+        case 3:
+        result = first * second; //multiply
+            break;
+
+        case 4:
+        result = first / second; //divide
+            break;
+
+        case 5:
+        result = (int) first % (int)second; //modulus.........ignore decimal number....convert it in integer then find modulo
+            break;
+
+        case 6:
+        result = pow(first, second); //power
+            break;
+
+        default:
+            break;
+        }
+
+        printf("\n Result of operation is: %.2f", result);
     };
     return 0;
 }
 
-void print_menu() {
+void print_menu()
+{
 
     printf("\n Welcome to simple Calculator!");
     printf("\n Choose one of the following options: ");
