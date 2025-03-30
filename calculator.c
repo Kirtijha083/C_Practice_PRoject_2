8453249e756910ec4f777ea5f653e8d0e6bacab0
KirtiJha <kirti.jha2025@gmail.com>
2025-03-30 15:19:11 +0530

    added some more logic

--- calculator.c
+++ calculator.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
-int main(){
+
+void print_menu(); // function(or ab sara menu isme add kr denge ...soo our main fun will more small)
+
+int main()
+{
+    int choice;
+    double first, second, result;
+
+    while(1) {
+        print_menu();
+        scanf("%d", &choice);
+if( choice == 7){
+    break;
+}
+        printf("\n Please enter the first number: ");
+        scanf("%lf", &first); //double = lf, float = f
+
+        printf("Now, enter the second number: ");
+        scanf("%lf", &second);
+    };
+    return 0;
+}
+
+void print_menu() {
+
     printf("\n Welcome to simple Calculator!");
     printf("\n Choose one of the following options: ");
     printf("\n1. Add");
@@ -9,5 +33,6 @@ int main(){
     printf("\n5. Modulus");
     printf("\n6. Power");
     printf("\n7. Exit");
-    return 0;
+
+    printf("\n Now enter your choice:");
 }
